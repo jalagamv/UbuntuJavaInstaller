a7a9e5c44c44055eb77fdc1844950915191c2b45
jalagamv <37338874+jalagamv@users.noreply.github.com>
2019-04-16 22:56:59 +0530

    Update ujavainstaller.sh

--- ujavainstaller.sh
+++ ujavainstaller.sh
@@ -134,11 +134,9 @@ echo "OK"
 # Update /etc/profile
 echo -n "Updating /etc/profile ... "
 cat >> /etc/profile <<EOF
-JAVA_HOME=$JDK_LOCATION
-JRE_HOME=$JDK_LOCATION/jre
-PATH=$PATH:$JDK_LOCATION/bin:$JDK_LOCATION/jre/bin
+JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
 export JAVA_HOME
-export JRE_HOME
 export PATH
 EOF
 echo "OK"
